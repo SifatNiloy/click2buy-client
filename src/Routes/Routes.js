0e9a6bc4adb69cf2bb049a919f84ca4b3aa3cf8d
SifatNiloy <sifatniloy18@gmail.com>
2023-03-13 22:52:27 +0600

    folder structure, nav, footer added

---
+++ src/Routes/Routes.js
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import Main from "../Pages/Layout/Main";
+import Login from "../Pages/Login/Login";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main/> ,
+        children: [
+            {
+                path:'/',
+                element: <Home/>,
+            },
+            {
+                path:'/login',
+                element:<Login/>,
+            }
+        ]
+    },
+]);
