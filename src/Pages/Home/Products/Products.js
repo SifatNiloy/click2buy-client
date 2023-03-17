50e1df881cd94c75fdeafea610bde435aacd7fcb
SifatNiloy <sifatniloy18@gmail.com>
2023-03-17 18:43:20 +0600

    login react hook form added

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -4,17 +4,28 @@ import Product from './Product';
 const Products = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('products.json')
+        fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => setProducts(data));
     }, []);
     return (
-        <div className='grid md:grid-cols-3 grid-gap-4'>
-           
-            {
-                products.map(product=><Product product={product} key={product.id} />)
-            }
+        <div>
+            <div className="form-control">
+                <div className="input-group">
+                    <input type="text" placeholder="Search…" className="input input-bordered" />
+                    <button className="btn btn-square">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+                </div>
+            </div>
+            <div className='grid md:grid-cols-3 grid-gap-4'>
+
+                {
+                    products.map(product => <Product product={product} key={product.id} />)
+                }
+            </div>
         </div>
+
     );
 };
 
