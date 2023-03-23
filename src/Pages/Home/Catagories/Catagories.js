50ab2b78a34924372e2932a2e9837d8f35734a94
SifatNiloy <sifatniloy18@gmail.com>
2023-03-23 23:40:22 +0600

    brands added

--- src/Pages/Home/Catagories/Catagories.js
+++ src/Pages/Home/Catagories/Catagories.js
@@ -9,32 +9,37 @@ const Catagories = () => {
         <div className='my-12 mx-12'>
             <div className='flex my-6'>
                 <h2 className="text-3xl font-bold">Explore Popular Catagories  |</h2>
-                <p className="text-2xl">See All <FontAwesomeIcon icon="fa-solid fa-arrow-right" /></p>
+                <p className="text-3xl ml-6"> See All <FontAwesomeIcon icon="fa-solid fa-arrow-right" /></p>
             </div>
             <div className='grid md:grid-cols-5 gap-10'>
                 <div>
                     <div className='rounded-products'>
                         <img src={sneckers} alt="" />
+                        <p>Sneckers</p>
                     </div>
                 </div>
                 <div>
                     <div className='rounded-products'>
                         <img src={watch} alt="" />
+                        <p>Watches</p>
                     </div>
                 </div>
                 <div>
                     <div className='rounded-products'>
                         <img src={smartphone} alt="" />
+                        <p>Smartphones</p>
                     </div>
                 </div>
                 <div>
                     <div className='rounded-products'>
                         <img src={sneckers} alt="" />
+                        <p>Sneckers</p>
                     </div>
                 </div>
                 <div>
                     <div className='rounded-products'>
                         <img src={watch} alt="" />
+                        <p>Watches</p>
                     </div>
                 </div>
                 
